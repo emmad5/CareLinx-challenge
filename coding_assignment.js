20879afe59b6f2cd92de460487e2f5ceb5133a3e
Emma Deas <emmadeas9@gmail.com>
2019-04-09 14:57:43 -0700

    work on assignment 2

--- coding_assignment.js
+++ coding_assignment.js
@@ -1,3 +1,4 @@
+//Assignment 1
 function amountToCoins(amt, set) {
     let result = [];
     let sum = 0;
@@ -13,3 +14,24 @@ function amountToCoins(amt, set) {
     return result;
 }
 
+//Assignment 2
+function getText(node) {
+   let queue = [node];
+   let text = '';
+    while (queue.length > 0) {
+      let n = queue.shift();
+      if (n.is_tex) {
+          text += string;
+      }
+      if (!n.children) {
+        continue;
+      }
+     for (var i = 0; i < n.children.length; i++) {
+        queue.push(n.children[i]);
+     }
+    }
+    return text;
+}
+function compareText(node1, node2) {
+    return getText(node1) == getText(node2);
+}
